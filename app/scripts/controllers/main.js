5e0cd09b754233875712adbe68ca5ba8f623a201
Giacomo Bresciani <brescia123@gmail.com>
2014-06-05 11:07:31 +0200

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('pomasanaAppApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
